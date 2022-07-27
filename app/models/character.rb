d618cc862753fa8b069df1557a1b09bbf3d69548
Kristine Kochie <kristinekochie@gmail.com>
2022-07-26 23:15:10 -0400

    commit tree 2e1bd3bcbbf2497fe3f8129bfef322657e0ef2c9

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    #define a method using a given character's catchphrase
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
 end
